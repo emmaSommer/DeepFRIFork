53389f9b650508df9bddbc5b7c014fc7f50c6ac5
Emma Sommerová <xsommer@fi.muni.cz>
2025-04-09 15:30:11 +0200

    fix missing code

--- preprocessing/biotoolbox/contact_map_builder.py
+++ preprocessing/biotoolbox/contact_map_builder.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Bio import Align
+#from Bio.Data.SCOPData import protein_letters_3to1
 from Bio.SeqUtils import seq1
 from Bio.SeqUtils import IUPACData  # <-- Use IUPACData instead of SCOPData
 
@@ -35,7 +36,6 @@ class ContactMapContainer:
 
 
 def correct_residue(x, target):
-    """Use IUPACData.protein_letters_3to1 to convert a three-letter code."""
     try:
         sl = IUPACData.protein_letters_3to1[x.resname]
         if sl == target:
@@ -49,7 +49,7 @@ class DistanceMapBuilder:
     def __init__(self,
                  atom="CA",
                  verbose=True,
-                 pedantic=True,
+                 pedantic=False,
                  glycine_hack=-1):
 
         self.verbose = verbose
@@ -77,7 +77,7 @@ class DistanceMapBuilder:
         return self.__atom
 
     def generate_map_for_pdb(self, structure_container):
-        #assert False
+
         aligner      = Align.PairwiseAligner()
         contact_maps = ContactMapContainer()
         model        = structure_container.structure[0]
@@ -94,7 +94,7 @@ class DistanceMapBuilder:
             if chain['seqres-seq'] is not None and len(chain['seqres-seq']) > 0:
                 contact_maps.with_method_for_chain(chain_name, ALIGNED_BY_SEQRES)
                 seqres_seq = chain['seqres-seq']
-                atom_seq   = chain['atom-seq']
+                atom_seq  = chain['atom-seq']
 
                 alignment = aligner.align(seqres_seq, atom_seq)
                 specific_alignment = next(alignment)
@@ -117,6 +117,7 @@ class DistanceMapBuilder:
                 picked_residues = 0
                 non_canonicals_or_het = 0
 
+                logging.info('aligned atom seq: ' + str(len(aligned_atom_seq)) + '\n' + str(aligned_atom_seq))
                 for i in range(len(aligned_atom_seq)):
                     if aligned_seqres_seq[i] == '-':
                         # This is an inserted residue from the aligner that doesn't actually match any
@@ -125,13 +126,8 @@ class DistanceMapBuilder:
                     current_aligned_atom_residue_letter = aligned_atom_seq[i]
                     #  atom seq has a letter and the mask shows it corresponds to a seqres item
                     if current_aligned_atom_residue_letter != '-' and mask[i] == '|':
-                        candidate_residue = next(
-                            (
-                                x for x in reindexed_residues[picked_residues : picked_residues + 5]
-                                if correct_residue(x, current_aligned_atom_residue_letter)
-                            ),
-                            None
-                        )
+                        candidate_residue = next((x for x in reindexed_residues[picked_residues:picked_residues + 5] if
+                                                  correct_residue(x, current_aligned_atom_residue_letter)), None)
 
                         if candidate_residue is None:
                             # The right answer is probably 'None' but we need to know why.
@@ -146,60 +142,46 @@ class DistanceMapBuilder:
                         final_residue_list.append(None)
 
                 final_seq_three_letter_codes = ''.join(
-                    [r.resname if r is not None else 'XXX' for r in final_residue_list]
-                )
-                # We can still call seq1, but specify our custom_map from IUPACData
-                final_seq_one_letter_codes = seq1(
-                    final_seq_three_letter_codes,
-                    undef_code='-',
-                    custom_map=IUPACData.protein_letters_3to1
-                )
-                self.speak(
-                    f"Final [len of seq {len(seqres_seq)}] "
-                    f"[len of result {len(final_seq_one_letter_codes)}] "
-                    f"[len of final residue list {len(final_residue_list)}]:\n{final_seq_one_letter_codes}"
-                )
+                    [r.resname if r is not None else 'XXX' for r in final_residue_list])
+                final_seq_one_letter_codes = seq1(final_seq_three_letter_codes, undef_code='-',
+                                                  custom_map=IUPACData.protein_letters_3to1)
+                self.speak(f"Final [len of seq {len(seqres_seq)}] [len of result {len(final_seq_one_letter_codes)}] "
+                           f"[len of final residue list {len(final_residue_list)}]:\n{final_seq_one_letter_codes}")
 
                 if self.pedantic and len(final_residue_list) != len(seqres_seq):
                     raise Exception(
-                        f"Somehow the final residue list {len(final_residue_list)} "
-                        f"doesn't match the size of the SEQRES seq {len(seqres_seq)}"
-                    )
+                        f"Somehow the final residue list {len(final_residue_list)} doesn't match the size of the SEQRES seq {len(seqres_seq)}")
 
                 if self.pedantic and (len(seqres_seq) != len(final_seq_one_letter_codes) != len(final_residue_list)):
                     raise Exception(
-                        'The length of the SEQRES seq != length of final_seq_one_letter_codes != '
-                        'length of final residue list'
-                    )
+                        'The length of the SEQRES seq != length of final_seq_one_letter_codes != length of final residue list')
 
                 sanity_check = aligned_atom_seq.replace('X', '')
                 if self.pedantic and sanity_check != final_seq_one_letter_codes:
                     print(f"sanity_check {sanity_check}")
                     print(f"final_seq    {final_seq_one_letter_codes}")
                     count = sum(1 for a, b in zip(sanity_check, final_seq_one_letter_codes) if a != b)
-                    missing_residue_heuristic   = sanity_check.count('-') / len(sanity_check)
+                    # While going through the data we found some _very_ large structures in the PDB.
+                    # Some of them have massive interior chains w/ tons of missing data. In this case
+                    # we're basically just saying we did what we could, passing the data along and saying
+                    # we still were in pedantic mode.
+                    missing_residue_heuristic = sanity_check.count('-') / len(sanity_check)
                     missing_residue_heuristic_2 = final_seq_one_letter_codes.count('-') / len(final_seq_one_letter_codes)
                     if count == non_canonicals_or_het:
+                        # Add a message about this.
                         print(
-                            "Warning: The final sequence and the sanity check were different, "
-                            "but the difference equals the number of HETATMs or non-canonical residues. _Probably_ OK."
-                        )
+                            f"Warning: The final sequence and the sanity check were different, but the difference equals the number of HETATMs or non-canonical residues. _Probably_ OK.")
                     elif missing_residue_heuristic >= 0.5 or missing_residue_heuristic_2 >= 0.5:
-                        print(
-                            "Warning: The final sequence and the sanity check were different. "
-                            "Over 50% of the chain is unresolved. Nothing we can do about it."
-                        )
+                        print(f"Warning: The final sequence and the sanity check were different. Over 50% of the chain is unresolved. Nothing we can do about it.")
                     else:
-                        print("Vlada")
-                        # raise Exception("The final one letter SEQ generated from residues does not match "
-                        #                 "the aligned atom seq (Diff count {count} but HETATM {non_canonicals_or_het})")
+                        print ("Vlada")
+                        # raise Exception(
+                        #    f'The final one letter SEQ generated from residues does not match the aligned atom seq (Diff count {count} but HETATM {non_canonicals_or_het})')
 
                 contact_maps.with_final_seq_for_chain(chain_name, final_seq_one_letter_codes)
                 contact_maps.with_chain_seq(chain_name, seqres_seq)
-                contact_maps.with_map_for_chain(
-                    chain_name,
-                    self.__residue_list_to_contact_map(final_residue_list, len(seqres_seq))
-                )
+                contact_maps.with_map_for_chain(chain_name,
+                                                self.__residue_list_to_contact_map(final_residue_list, len(seqres_seq)))
             else:
                 contact_maps.with_method_for_chain(chain_name, ATOMS_ONLY)
                 atom_seq = chain['atom-seq']
@@ -216,29 +198,23 @@ class DistanceMapBuilder:
 
                 # Sanity checks
                 final_seq_three_letter_codes = ''.join(
-                    [r.resname if r is not None else 'XXX' for r in final_residue_list]
-                )
-                final_seq_one_letter_codes = seq1(
-                    final_seq_three_letter_codes,
-                    undef_code='-',
-                    custom_map=IUPACData.protein_letters_3to1
-                )
+                    [r.resname if r is not None else 'XXX' for r in final_residue_list])
+                final_seq_one_letter_codes = seq1(final_seq_three_letter_codes, undef_code='-',
+                                                  custom_map=IUPACData.protein_letters_3to1)
                 print(final_seq_one_letter_codes)
                 corrected_atom_seq = final_seq_one_letter_codes
                 # End sanity checks
 
                 contact_maps.with_chain_seq(chain_name, corrected_atom_seq)
-                contact_maps.with_map_for_chain(
-                    chain_name,
-                    self.__residue_list_to_contact_map(final_residue_list, len(corrected_atom_seq))
-                )
+                contact_maps.with_map_for_chain(chain_name,
+                                                self.__residue_list_to_contact_map(final_residue_list, len(corrected_atom_seq)))
 
         return contact_maps
 
     def __residue_list_to_contact_map(self, residue_list, length):
         dist_matrix = self.__calc_dist_matrix(residue_list)
         diag = self.__diagnolize_to_fill_gaps(dist_matrix, length)
-        # contact_map = self.__create_adj(diag, TEN_ANGSTROMS)
+        #contact_map = self.__create_adj(diag, TEN_ANGSTROMS)
         contact_map = diag
         return contact_map
 
@@ -258,7 +234,7 @@ class DistanceMapBuilder:
         A = _A.copy()
         with np.errstate(invalid='ignore'):
             A[A <= thresh] = 1.0
-            A[A > thresh]  = 0.0
+            A[A > thresh] = 0.0
             A[np.isnan(A)] = 0.0
             A = self.__norm_adj(A)
         return A
@@ -303,3 +279,14 @@ class DistanceMapBuilder:
                 except IndexError:
                     pass
         return A
+
+    def __calc_dist_matrix(self, chain_one):
+        """Returns a matrix of C-alpha distances between two chains"""
+        answer = np.zeros((len(chain_one), len(chain_one)), np.float)
+        for row, residue_one in enumerate(chain_one):
+            for col, residue_two in enumerate(chain_one[row:], start=row):
+                if col >= len(chain_one):
+                    continue  # enumerate syntax is convenient, but results in invalid indices on last column
+                answer[row, col] = self.__calc_residue_dist(residue_one, residue_two)
+                answer[col, row] = answer[row, col]  # cchandler
+        return answer
